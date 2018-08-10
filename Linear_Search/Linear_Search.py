cd1c6de1c4b7e45dccb24ce49dc5ed862b20b988
finite <finite.impulse@gmail.com>
2018-08-10 08:28:53 +0200

    Linear Search

--- Linear_Search/Linear_Search.py
+++ Linear_Search/Linear_Search.py
@@ -7,10 +7,6 @@ def unordered_linear_search(arr, value_to_search):
             return i
     return -1
 
-# Test1
-a = [534, 246, 933, 127, 277, 321, 454, 565, 220]
-print(unordered_linear_search(a, 277))
-
 # Sorted Linear Search
 def ordered_linear_search(arr, value_to_search):
     for i in range(len(arr)):
