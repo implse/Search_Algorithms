e7d10de583c9095ca1f62170c783c680c66f2b67
Finite <finite.impulse@gmail.com>
2019-07-25 05:04:43 +0200

    Update variable name

--- Linear_Search/Linear_Search.py
+++ Linear_Search/Linear_Search.py
@@ -1,17 +1,17 @@
 # Linear Search or Sequential Search : Time Complexity O(n)
 
 # Unordered Linear Search
-def unordered_linear_search(arr, value_to_search):
+def unordered_linear_search(arr, target_value):
     for i in range(len(arr)):
-        if arr[i] == value_to_search:
+        if arr[i] == target_value:
             return i
     return -1
 
 # Sorted Linear Search
-def ordered_linear_search(arr, value_to_search):
+def ordered_linear_search(arr, target_value):
     for i in range(len(arr)):
-        if arr[i] == value_to_search:
+        if arr[i] == target_value:
             return i
-        elif arr[i] > value_to_search:
+        elif arr[i] > target_value:
             return -1
     return -1
